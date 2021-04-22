9ced2d08f917e13a6c41f9ceed93b681d7b0b3e6
Aroita <aroitaim84@gmail.com>
2021-04-22 18:52:39 +0200

    practica_1_acabada

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
 
+ $(function () {
+          $(".bt-delet").click(function() {
 
-//eliminar filas con botton borrar
+            if(confirm("¿estas seguro?")) {
+              $(this).parents('tr').hide();
+
+            }
+        });
+
+        });
+
+/* eliminar filas con botton borrar
 function eliminar(id){
   var row = document.getElementById("");
 
   document.getElementById("table").deleteRow(row);
 }
-
+*/
 
 
 
